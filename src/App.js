7a9d654fe01157497a14c0cc7f50f0dcb92149c4
Rohit Singh <singh.rohitsingh2k@gmail.com>
2022-04-12 13:29:09 +0530

    Code commented.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,43 @@
-import logo from "./logo.svg";
 import "./App.css";
-import StripeCheckout from "react-stripe-checkout";
+import logo from "./logo.svg";
 import { useState } from "react";
+import StripeCheckout from "react-stripe-checkout";
 import { SERVER_BASE_URL, STRIPE_PROD_KEY } from "./config";
 
 const KEY = STRIPE_PROD_KEY;
-console.log(KEY)
 
 function App() {
-
+  // We can modify this state as per our requirements.
   const [product, setProduct] = useState({
     name: "Pick Axe",
     price: 10,
     productBy: "Cong Holistic",
   });
 
+  // This function proceeds the payments.
   const makePayment = (token) => {
     const body = {
-      token, 
-      product
-    }
+      token,
+      product,
+    };
     const headers = {
-      "Content-Type" : "application/json"
-    }
+      "Content-Type": "application/json",
+    };
 
-    return fetch (`${SERVER_BASE_URL}/api/checkout/payment`, {
-      method : 'POST',
+    return fetch(`${SERVER_BASE_URL}/api/checkout/payment`, {
+      method: "POST",
       headers,
-      body : JSON.stringify(body)
-    }).then( res => {
-      console.log("Response : ", res)
-      const {status} = res
-      console.log("STATUS : ", status)
-    }).catch(err => {
-      console.log(err);
+      body: JSON.stringify(body),
     })
-  }
+      .then((res) => {
+        console.log("Response : ", res);
+        const { status } = res;
+        console.log("STATUS : ", status);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <div className="App">
@@ -50,13 +52,17 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+          {/* Here we are using stripe component This looks like a button */}
           <StripeCheckout
             stripeKey={KEY}
             token={makePayment}
             name="buy me"
-            amount = {product.amount * 100}
+            amount={product.amount * 100}
           >
-            <button onClick={(e)=> e.preventDefault()}> My custom button</button>
+            <button onClick={(e) => e.preventDefault()}>
+              {" "}
+              My custom button
+            </button>
           </StripeCheckout>
         </a>
       </header>
